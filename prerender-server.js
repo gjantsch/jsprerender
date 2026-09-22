6905b0bc41a090d232570c75f510429a24ba33a3
Gustavo Jantsch <gustavo.jantsch@sap.com>
2026-09-22 10:38:37 -0300

    Replace blocking file I/O with fs.promises on the hot path
    fs.statSync, fs.existsSync, fs.readFileSync, and fs.writeFileSync all
    block the Node.js event loop. Converted fileOlderThan to async and
    replaced all sync calls in the request handler with their fs.promises
    equivalents so other requests can be processed while cache I/O is pending.

--- prerender-server.js
+++ prerender-server.js
@@ -12,10 +12,10 @@ const util = require('util')
  * @param {*} duration 
  * @returns 
  */
-const fileOlderThan = (filename, duration) => {
+const fileOlderThan = async (filename, duration) => {
     let stats
     try {
-        stats = fs.statSync(filename)
+        stats = await fs.promises.stat(filename)
     } catch (e) {
         return true
     }
@@ -141,15 +141,20 @@ app.get('*', async (req, res) => {
     const fileName = `${config.cache.directory}/${fileHash}`;
 
     let html = '';
+    let cacheExists = false;
+    try {
+        await fs.promises.access(fileName);
+        cacheExists = true;
+    } catch { }
 
-    if (fs.existsSync(fileName) && !fileOlderThan(fileName, config.cache.ttl)) {
+    if (cacheExists && !(await fileOlderThan(fileName, config.cache.ttl))) {
         logger(`Reading from cache ${fileName}`);
-        html = fs.readFileSync(fileName);
+        html = await fs.promises.readFile(fileName, 'utf8');
     } else {
         html = await getPage(pageURL);
         if (html.length >= config.cache.minContentSize && pageURL.indexOf('debug') === -1) {
             logger(`Writing to cache ${fileName}`);
-            fs.writeFileSync(fileName, html);
+            await fs.promises.writeFile(fileName, html);
         }
     }
 
